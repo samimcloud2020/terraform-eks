c6b368d5ab3fae70ecba581474522a6ecdaedeb1
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-01-21 11:45:19 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -28,11 +28,11 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
 }
 
 resource "aws_eks_cluster" "aws_eks" {
-  name     = "eks_cluster_tuto"
+  name     = "eks_cluster_samim"
   role_arn = aws_iam_role.eks_cluster.arn
 
   vpc_config {
-    subnet_ids = ["subnet-0d7a0ae0b943e240f", "subnet-0622e286cdbb9dacc"]
+    subnet_ids = ["subnet-062ac4058a80adee1", "subnet-0fbfc73525169a2de"]
   }
 
   tags = {
@@ -78,7 +78,7 @@ resource "aws_eks_node_group" "node" {
   cluster_name    = aws_eks_cluster.aws_eks.name
   node_group_name = "node-group1"
   node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = ["subnet-0d7a0ae0b943e240f", "subnet-0622e286cdbb9dacc"]
+  subnet_ids      = ["subnet-062ac4058a80adee1", "subnet-0fbfc73525169a2de"]
 
   scaling_config {
     desired_size = 1
